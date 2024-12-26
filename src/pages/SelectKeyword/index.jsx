04b10647d5d4169669c4e34e59cb117d7f24bd8a
yujunggg <a90134627@gmail.com>
2024-12-26 15:32:55 +0900

    feat:append category

--- src/pages/SelectKeyword/index.jsx
+++ src/pages/SelectKeyword/index.jsx
@@ -1,12 +1,50 @@
-import React from 'react'
-import * as S from './style'
+import React from "react";
+import * as S from "./style";
 
 const SelectKeyword = () => {
   return (
     <div>
-       <S.FoodQuestion>어떤 음식을 원하시나요?</S.FoodQuestion>
+      <div>
+        <S.FoodQuestion>어떤 음식을 원하시나요?</S.FoodQuestion>
+        <S.RestaurantRecommendations>
+          정해진 키워드에 맞춰 식당을 추천해드려요
+        </S.RestaurantRecommendations>
+      </div>
+      <div>
+        <S.Keyword>맛</S.Keyword>
+        <hr
+          style={{
+            border: "none",
+            height: "1px",
+            backgroundColor: "#979797",
+            margin: "10px 40px",
+          }}
+        />
+      </div>
+      <div>
+        <S.Keyword>음식 종류</S.Keyword>
+        <hr
+          style={{
+            border: "none",
+            height: "1px",
+            backgroundColor: "#979797",
+            margin: "10px 40px",
+          }}
+        />
+      </div>
+      <div>
+        <S.Keyword>기타</S.Keyword>
+        <hr
+          style={{
+            border: "none",
+            height: "1px",
+            backgroundColor: "#979797",
+            margin: "10px 40px",
+          }}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SelectKeyword
+export default SelectKeyword;
--- src/pages/SelectKeyword/style.js
+++ src/pages/SelectKeyword/style.js
@@ -1,17 +1,25 @@
 import styled from "styled-components";
 import { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'yg-jalnan';
-    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_four@1.2/JalnanOTF00.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
-  }
-`;
-
 
 export const FoodQuestion = styled.div`
-    font-size: 24px;
-    font-family: 'yg-jalnan', sans-serif;
+  font-size: 24px;
+  margin-left: 40px;
+  padding-top: 130px;
+  font-family: 'yg-jalnan', sans-serif;
+`
+
+export const RestaurantRecommendations = styled.div`
+  font-size: 16px;
+  margin-left: 40px;
+  margin-top: 15px;
+  font-family: 'yg-jalnan', sans-serif;
+`
+export const Keyword = styled.div`
+  margin-left: 40px;
+  font-size: 18px;
+  font-family: Pretendard;
+  margin-top: 40px;
+  color: #979797;
+  font-weight: bold;
 `
