fb4370e0c0319930aa11a316adf16c0166be32bd
yujunggg <a90134627@gmail.com>
2024-12-26 17:12:10 +0900

    feat:button setting

--- src/pages/SelectKeyword/index.jsx
+++ src/pages/SelectKeyword/index.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import * as S from "./style";
+import backbutton from '../../asset/back.svg'
+import { Navigate, useNavigate } from "react-router-dom";
 
 const SelectKeyword = () => {
+  const navigate = useNavigate();
+
+  function handleClick(){
+    navigate('/')
+  }
+
   return (
     <div>
+      <S.backbutton src={backbutton} onClick={handleClick}></S.backbutton>
       <div>
         <S.FoodQuestion>어떤 음식을 원하시나요?</S.FoodQuestion>
         <S.RestaurantRecommendations>
@@ -20,6 +29,18 @@ const SelectKeyword = () => {
             margin: "10px 40px",
           }}
         />
+        <S.KeywordButtonContainer>
+          <S.KeywordButton>담백한</S.KeywordButton>
+          <S.KeywordButton>칼칼한</S.KeywordButton>
+          <S.KeywordButton>매콤한</S.KeywordButton>
+          <S.KeywordButton>고소한</S.KeywordButton>
+          <S.KeywordButton>짭짤한</S.KeywordButton>
+          <S.KeywordButton>얼큰한</S.KeywordButton>
+          <S.KeywordButton>시원한</S.KeywordButton>
+          <S.KeywordButton>새콤한</S.KeywordButton>
+          <S.KeywordButton>달달한</S.KeywordButton>
+          <S.KeywordButton>쌉쌀한</S.KeywordButton>
+        </S.KeywordButtonContainer>
       </div>
       <div>
         <S.Keyword>음식 종류</S.Keyword>
@@ -31,6 +52,18 @@ const SelectKeyword = () => {
             margin: "10px 40px",
           }}
         />
+        <S.KeywordButtonContainer>
+          <S.KeywordButton>양식</S.KeywordButton>
+          <S.KeywordButton>중식</S.KeywordButton>
+          <S.KeywordButton>한식</S.KeywordButton>
+          <S.KeywordButton>일식</S.KeywordButton>
+          <S.KeywordButton>브런치</S.KeywordButton>
+          <S.KeywordButton>동남아</S.KeywordButton>
+          <S.KeywordButton>치킨</S.KeywordButton>
+          <S.KeywordButton>피자</S.KeywordButton>
+          <S.KeywordButton>햄버거</S.KeywordButton>
+          <S.KeywordButton>야식</S.KeywordButton>
+        </S.KeywordButtonContainer>
       </div>
       <div>
         <S.Keyword>기타</S.Keyword>
@@ -42,7 +75,16 @@ const SelectKeyword = () => {
             margin: "10px 40px",
           }}
         />
+        <S.KeywordButtonContainer>
+          <S.KeywordButton>요즘 유행</S.KeywordButton>
+          <S.KeywordButton>10대의</S.KeywordButton>
+          <S.KeywordButton>20대의</S.KeywordButton>
+          <S.KeywordButton>주차 가능한</S.KeywordButton>
+        </S.KeywordButtonContainer>
       </div>
+      <S.SelectButtonContainer>
+        <S.SelectButton>선택 완료</S.SelectButton>
+      </S.SelectButtonContainer>
     </div>
   );
 };
--- src/pages/SelectKeyword/style.js
+++ src/pages/SelectKeyword/style.js
@@ -1,25 +1,66 @@
 import styled from "styled-components";
 import { createGlobalStyle } from 'styled-components';
 
+export const backbutton = styled.img`
+  margin-left: 25px;
+  margin-top: 50px;
+`
 
 export const FoodQuestion = styled.div`
-  font-size: 24px;
+  font-size: 1.6em;
   margin-left: 40px;
-  padding-top: 130px;
+  padding-top: 40px;
   font-family: 'yg-jalnan', sans-serif;
 `
 
 export const RestaurantRecommendations = styled.div`
-  font-size: 16px;
+  font-size: 1.1em;
   margin-left: 40px;
   margin-top: 15px;
   font-family: 'yg-jalnan', sans-serif;
 `
 export const Keyword = styled.div`
   margin-left: 40px;
-  font-size: 18px;
+  font-size: 1.2em;
   font-family: Pretendard;
   margin-top: 40px;
   color: #979797;
   font-weight: bold;
+`
+export const KeywordButton = styled.button`
+  border-radius: 10px;
+  width: 6vw;
+  height: 4vh;
+  border: none;
+  font-size: 0.9em;
+  background-color: #FFDCD4;
+  cursor: pointer;
+`
+
+export const KeywordButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 20px;
+  margin-top: 20px;
+`
+
+export const SelectButton = styled.button`
+  margin-top: 60px;
+  width: 35.7vw;
+  height: 6vh;
+  border: none;
+  border-radius: 20px;
+  color: white;
+  background-color: #FF633C;
+  font-size: 18px;
+  font-weight: bold;
+  cursor: pointer;
+`
+
+export const SelectButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
 `
