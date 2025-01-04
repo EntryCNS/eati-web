39d57781488270b2d4b48b50f5b985480930d46d
yujunggg <a90134627@gmail.com>
2025-01-04 15:31:21 +0900

    feat:creatingGroup view

--- src/pages/CreatingGroup/index.jsx
+++ src/pages/CreatingGroup/index.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import * as S from './style'
+import mainCharacterImg from '../../asset/main.png'
+import backbutton from "../../asset/back.svg";
+import { useNavigate } from 'react-router-dom';
 
 const CreatingGroup = () => {
+  const navigate = useNavigate();
+
+  function handleBackButton() {
+    navigate("/");
+  }
+
   return (
     <div>
-      <div>키워드 고르는 중...</div>
+      <S.backbutton src={backbutton} onClick={handleBackButton}></S.backbutton>
+      <S.SelectKeyword>키워드 고르는 중...</S.SelectKeyword>
+      <S.MainCharacterContainer>
+        <S.MainCharacter src={mainCharacterImg}></S.MainCharacter>
+      </S.MainCharacterContainer>
     </div>
   )
 }
--- src/pages/CreatingGroup/style.js
+++ src/pages/CreatingGroup/style.js
@@ -1,1 +1,25 @@
 import styled from "styled-components";
+
+export const SelectKeyword = styled.div`
+    font-size: 20px;
+    text-align: center;
+    font-family: 'yg-jalnan', sans-serif;
+    margin-top: 100px;
+`
+
+export const MainCharacter = styled.img`
+    margin-top: 20px;
+    align-items: center;
+`
+
+export const MainCharacterContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+`
+
+export const backbutton = styled.img`
+  margin-left: 25px;
+  margin-top: 50px;
+`
