d57f36258a2ae16a11b033479a1248366ecc04ce
tpqls774 <tpqls774@dgsw.hs.kr>
2025-02-05 08:50:46 +0900

    feat: 서버연결

--- src/pages/StoreInfo/index.jsx
+++ src/pages/StoreInfo/index.jsx
@@ -3,50 +3,48 @@ import { useParams } from 'react-router-dom';
 import * as S from './style'
 
 const StoreInfo = () => {
-    // const { id } = useParams();
-    // const [product, setProduct] = useState(null);
-    // const [loading, setLoading] = useState(true);
-    // const [error, setError] = useState(null);
+    const { id } = useParams();
+    const [product, setProduct] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-    // useEffect(() => {
-    //     const fetchProduct = async () => {
-    //         try {
-    //             const response = await fetch(`http://10.80.163.17:8080/api/restaurants/${id}`);
-    //             if (!response.ok) {
-    //                 throw new Error('불러오기 실패')
-    //             }
-    //             const data = await response.json();
-    //             setProduct(data);
-    //         } catch(error) {
-    //             setError(error.message);
-    //         } finally {
-    //             setLoading(false);
-    //         }
-    //     };
+    useEffect(() => {
+        const fetchProduct = async () => {
+            try {
+                const response = await fetch(`http://10.80.163.17:8080/api/restaurants/${id}`);
+                if (!response.ok) {
+                    throw new Error('불러오기 실패')
+                }
+                const data = await response.json();
+                setProduct(data);
+            } catch(error) {
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
 
-    //     fetchProduct();
-    // }, [id]);
+        fetchProduct();
+    }, [id]);
 
-    // if(loading) return <p>loading....</p>
-    // if(error) return <p>error: {error}</p>
-    // if(!product) return <p>product not found</p>
+    if(loading) return <p>loading....</p>
+    if(error) return <p>error: {error}</p>
+    if(!product) return <p>product not found</p>
 
     return (
         <S.Wrap>
-            <S.Title>가게이름: 블라블라가게</S.Title>
+            <S.Title>가게이름: {product.name}</S.Title>
             <S.Content>
-                <S.Text>가게주소: 블라블라주소</S.Text>
+                <S.Text>가게주소: {product.address}</S.Text>
                 <S.Text>키워드: 매콤한</S.Text>
-                <S.Text>전화번호: 0803350020</S.Text>
+                <S.Text>전화번호: {product.phoneNumber}</S.Text>
                 <S.Text>리뷰</S.Text>
                 <S.Review>
                     <div style={{display: "flex", gap: "10px"}}>
-                        <S.ReviewText>글쓴이</S.ReviewText>
-                        <p>시간</p>
+                        <S.ReviewText>{product.reviews.authorName}</S.ReviewText>
+                        <p>{product.reviews.time}</p>
                     </div>
-                    <S.ReviewText>내용내용내용내용내용내용내용내용내용내용내용내용
-                        내용내용내용내용내용내용내용내용내용내용내용내용내용
-                    </S.ReviewText>
+                    <S.ReviewText>{product.review.text}</S.ReviewText>
                 </S.Review>
             </S.Content>
         </S.Wrap>
